e70579abf3942f7f451c744b820093dad33bc374
Darkness <kmd36rk@gmail.com$>
2020-03-06 22:50:24 +0700

    CI

---
+++ build-hal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source halium.env
+cd $ANDROID_ROOT
+
+source build/envsetup.sh
+export USE_CCACHE=1
+breakfast $DEVICE
+make -j$(nproc) halium-boot
+make -j$(nproc) recoveryimage
+make -j$(nproc) systemimage 
+
+echo "md5sum halium-boot.img and system.img"
+md5sum $ANDROID_ROOT/out/target/product/rolex/halium-boot.img
+md5sum $ANDROID_ROOT/out/target/product/rolex/recovery.img
+md5sum $ANDROID_ROOT/out/target/product/rolex/system.img

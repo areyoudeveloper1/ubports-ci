d114d67550dc55370defbd5758db4b81fe9c99ec
Darkness Hidden <kmd36rk@gmail.com>
2020-05-29 14:42:19 +0700

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -5,6 +5,8 @@ cd $ANDROID_ROOT
 source build/envsetup.sh
 export USE_CCACHE=1
 breakfast $DEVICE
+make -j$(nproc) mkbootimg
+make -j$(nproc) hybris-boot
 make -j$(nproc) halium-boot
 make -j$(nproc) recoveryimage
 make -j$(nproc) systemimage 
